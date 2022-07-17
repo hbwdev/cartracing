99622a07d3500db0f15634ac09e6ded8330f2db3
yammy <yammy@197887>
2022-07-17 12:35:33 -0400

    Disabled stats

--- js/lib/game.js
+++ js/lib/game.js
@@ -184,8 +184,8 @@ if (typeof navigator !== 'undefined') {
 
 		var stats = new Stats();
 		// Stats for performance debugging
-		stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-		document.body.appendChild(stats.dom);
+		//stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+		//document.body.appendChild(stats.dom);
 
 		that.draw = function () {
 			stats.begin();
